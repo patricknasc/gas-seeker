48da24341ebc5b230a8a874ba46ca7cafc59dda7
patricknasc <patrick.nascimento@gmail.com>
2016-11-22 16:23:01 -0200

    Table View constraints + outlets ref creation

--- Postos/PostoTableViewCell.h
+++ Postos/PostoTableViewCell.h
@@ -10,4 +10,18 @@
 
 @interface PostoTableViewCell : UITableViewCell
 
+@property (weak, nonatomic) IBOutlet UIImageView *CellImageView;
+
+@property (weak, nonatomic) IBOutlet UILabel *CellLblName;
+
+@property (weak, nonatomic) IBOutlet UILabel *etnLblPrice;
+
+@property (weak, nonatomic) IBOutlet UILabel *gasLblPrice;
+
+@property (weak, nonatomic) IBOutlet UILabel *gadLblPrice;
+
+@property (weak, nonatomic) IBOutlet UILabel *oilLblPrice;
+
+@property (weak, nonatomic) IBOutlet UIButton *goToBtn;
+
 @end
